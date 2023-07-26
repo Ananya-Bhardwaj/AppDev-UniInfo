bd7464361e6f173f22156627d5fddfc90ab727d9
Ananya <ananya.bhar@gmail.com>
2023-07-26 18:00:16 +0530

    added initial code to home

--- application/lib/api.dart
+++ application/lib/api.dart
@@ -10,5 +10,6 @@ getDataFromInternet() async {
   var url = Uri.parse('http://universities.hipolabs.com/search?country=india');
   var res = await http.get(url);
   var resobj = jsonDecode(res.body);
+  //resobj is a list of map<string, dynamic>
   print(resobj);
 }
--- application/lib/home.dart
+++ application/lib/home.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
+  static var countryName; 
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -11,7 +11,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text('University Country Database'),
+          Text('Search for universities located in your choice of country'), 
+          TextField(
+            onChanged: (value) => {
+              HomePage.countryName = value
+            }, 
+            decoration: InputDecoration(
+              labelText: 'Country Name', 
+              hintText: 'Enter Country Name!', 
+              border: InputBorder.none,
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, 'result');
+              },
+              child: Text('Check Results =>')),
+        ],
+      ),
     );
   }
 }
