1e4d0fa2294ac65a2cbb02ef3e0ccf680af14989
Ananya <ananya.bhar@gmail.com>
2023-07-30 11:17:29 +0530

    Made changes

--- application/lib/home.dart
+++ application/lib/home.dart
@@ -11,21 +11,44 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('UnivInfo'),
+        leading: Icon(Icons.school),
+      ),
+      body: Container(
+          height: 400,
+          width: 400,
+          margin: EdgeInsets.symmetric(horizontal: 5),
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+        alignment: AlignmentDirectional.topCenter,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('University Country Database'),
-          Text('Search for universities located in your choice of country'), 
+          Text('Search for universities located in any country',
+          style: TextStyle(
+            fontSize: 20, 
+            fontWeight: FontWeight.bold, 
+
+          ),
+          textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 50,),
           TextField(
             onChanged: (value) => {
-              HomePage.countryName = value
+              HomePage.countryName = value.trim()
             }, 
             decoration: InputDecoration(
               labelText: 'Country Name', 
               hintText: 'Enter Country Name!', 
               border: InputBorder.none,
+              contentPadding: EdgeInsets.all(5),
+              filled: true, 
+              fillColor: Colors.blue[50],
             ),
           ),
+          SizedBox(height: 50,),
           ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, 'result');
@@ -33,6 +56,7 @@ class _HomePageState extends State<HomePage> {
               child: Text('Check Results =>')),
         ],
       ),
+      )
     );
   }
 }
--- application/lib/result.dart
+++ application/lib/result.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+import 'package:application/home.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ResultPage extends StatefulWidget {
   const ResultPage({super.key});
@@ -13,42 +17,108 @@ class _ResultPageState extends State<ResultPage> {
   var listc = 0;
   var resobj = [];
 
-  getDataFromInternet() async {
-    var url = Uri.parse('http://universities.hipolabs.com/search?country=india');
-    var res = await http.get(url);
-    //resobj is a list of map<string, dynamic>
-    var resobj = jsonDecode(res.body);
-    // print(resobj[0]["web_pages"][0]);
-    setState(() {
-      listc = resobj.length;
-    });
-    print(listc);
-  }
-
   @override
   void initState() {
     //to run the function getData on opening this page
-    getDataFromInternet(); 
+    print('result page open');
+    getDataFromInternet();
     // TODO: implement initState
     super.initState();
   }
 
+  getDataFromInternet() async {
+    var url = Uri.parse(
+        'http://universities.hipolabs.com/search?country=${HomePage.countryName}');
+    var res = await http.get(url);
+    //resobj is a list of map<string, dynamic>
+    resobj = jsonDecode(res.body);
+    // print(resobj[0]["web_pages"][0]);
+    setState(() {
+      listc = resobj.length;
+    });
+    if (listc == 0) {
+      //no items
+    }
+  }
+
+  // Future<void> _lURL (index) async{
+  //   var url = Uri.parse(resobj[index]['web_pages'][0]);
+  //     if (!await launchUrl(url)) {
+  //   throw Exception('Could not launch $url');
+  // }
+
+  // }
+
+  // openLink(int index) async {
+  //   var url = resobj[index]['web_pages'][0];
+  //   if (await canLaunch(url)) {
+  //       await launch(url);
+  // } else {
+  //  throw 'Could not launch $url';
+  // }
+  // }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Results'),
+      ),
       body: ListView.builder(
           itemCount: listc,
-          itemBuilder: (context, index) {
+          itemBuilder: (BuildContext context, int index) {
             return Container(
               child: Container(
+                padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
                 child: Card(
-                    child: Column(
-                  children: [
-                    Text(resobj[index]['name']),
-                  ],
-                )),
+                    color: Colors.white,
+                    child: Container(
+                      child: Column(
+                      children: [
+                        Text(
+                          resobj[index]['name'],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Column(
+                              children: [
+                                Text(resobj[index]['state-province'] != null
+                            ? resobj[index]['state-province']
+                            : ''),
+                        Text(resobj[index]['country']),
+                              ],
+                            ),
+                            Expanded(child: Container()),
+                            IconButton(
+                            onPressed: () async {
+                              Uri url = Uri.parse(resobj[index]['web_pages'] != null
+                            ? resobj[index]['web_pages'][0]
+                            : '');
+                              if (!await launchUrl(url)) {
+                                print("link cannot be opened");
+                              } else {
+                                print('launch link');
+                                await launchUrl(url);
+                              }
+                            },
+                            icon: Icon(Icons.arrow_outward_outlined)),
+                      ],
+                    ),
+                      ],
+                        )
+                        ,
+                      padding: EdgeInsets.all(5),
+                    )
+                    
               ),
-            );
+              )
+            ); 
           }),
     );
   }
